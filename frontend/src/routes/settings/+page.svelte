75d45c0062258a755c215a6ba7129fe1865d2f6b
An-Owlbear <AnOwlbear@protonmail.com>
2025-04-13 00:28:53 +0100

    Added links to settings subpages on main page

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { Heading } from "flowbite-svelte";
+	import SettingLink from './SettingLink.svelte';
+	import { ArchiveSolid, InfoCircleSolid, UsersSolid } from 'flowbite-svelte-icons';
 
 </script>
 
-<Heading tag="h1">Settings</Heading>
+<Heading tag="h1" class="mb-8">Settings</Heading>
+
+<ul class="space-y-4">
+	<SettingLink href="/settings/users" title="User settings" Icon={UsersSolid} />
+	<SettingLink href="/settings/apps" title="App settings" Icon={ArchiveSolid}  />
+	<SettingLink href="/settings/system" title="System settings" Icon={InfoCircleSolid} />
+</ul>
---
+++ frontend/src/routes/settings/SettingLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+	import type { Component, Snippet } from 'svelte';
+
+	const { href, Icon, title }: {
+		href: string;
+		Icon: Component;
+		title: string;
+	} = $props();
+</script>
+
+<Card horizontal class="max-w-none space-x-10 flex flex-row items-center" href={href}>
+	<Icon class="w-15 h-15" />
+	<span class="text-2xl font-semibold">{title}</span>
+</Card>
