175c02234c40c78e3d4700479d4c0af1cc3250cd
An-Owlbear <AnOwlbear@protonmail.com>
2025-04-14 23:43:43 +0100

    Updated nat test to skip unless flag is set

--- backend/internal/networking/nat_test.go
+++ backend/internal/networking/nat_test.go
@@ -2,14 +2,21 @@ package networking
 
 import (
 	"context"
+	"flag"
 	"testing"
 
 	"github.com/An-Owlbear/homecloud/backend/internal/config"
 )
 
+var nat = flag.Bool("nat", false, "Whether or not to run nat tests")
+
 // IMPORTANT - this test will likely be inconsistent depending on your network
 // If your network doesn't support uPnP or NAT-PMP this won't work
 func TestTryMapPort(t *testing.T) {
+	if !*nat {
+		t.Skip("Skipping tests when -nat is disabled")
+	}
+
 	err := TryMapPort(context.Background(), 13231, 1323, config.NewDeviceConfig())
 	if err != nil {
 		t.Errorf("TestTryMapPort failed: %v", err)
